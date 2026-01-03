c7a62cb94d26d2b2837ff1d020007625d8b13b3b
akshay7745 <akshay7745@gmail.com>
2026-01-03 20:07:07 +0530

    implement middleware with route and method

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
 
-app.use("/", (req, res, next) => {
-  console.log("In the middleware");
-
-  next();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/add-product", (req, res, next) => {
+  res.send(`<form method="post" action="/products" >
+            <input type="text" placeholder="Enter product name" name="title"/>
+    <input type="text" name="size" placeholder="Provide product size"/>
+    <button type="submit">Submit</button>
+    </form>`);
 });
 
-app.use((req, res) => {
-  console.log("In the 2nd middleware...");
-  res.send({ data: "Hello I am coming as a json response..." });
+app.post("/products", (req, res) => {
+  console.log(req.body);
+  res.redirect("/add-product");
 });
-
 app.listen(3000, () => {
   console.log("Server is up and running...");
 });
